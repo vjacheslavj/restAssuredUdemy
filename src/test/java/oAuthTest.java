d6e2a26a39aa525e7e532e06bae2bf7f73a7d939
Vjaceslavs Jermakovs <vjacheslav.jermakov@gmail.com>
2023-05-09 13:43:34 +0300

    addition

--- src/test/java/oAuthTest.java
+++ src/test/java/oAuthTest.java
@@ -7,26 +7,27 @@ import org.openqa.selenium.WebDriver;
 import io.restassured.path.json.JsonPath;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public  class oAuthTest {
+public class oAuthTest {
 
     public static void main(String[] args) throws InterruptedException {
         // TODO Auto-generated method stub
-        System.setProperty("webdriver.chrome.driver", "/Users/vjaceslavsjermakovs/IdeaProjects/chrome driver/chromedriver113");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&auth_url=https://accounts.google.com/o/oauth2/v2/auth&client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com&response_type=code&redirect_uri=https://rahulshettyacademy.com/getCourse.php");
-        driver.findElement(By.cssSelector("input[type='email']")).sendKeys("test.udemy14@gmail.com");
-        driver.findElement (By.cssSelector("input [type='email']")).sendKeys (Keys. ENTER);
-        Thread.sleep (3000);
-        driver.findElement(By.cssSelector("input [type='password']")).sendKeys ("Password4512");
-        driver.findElement (By.cssSelector("input [type='password']")). sendKeys (Keys. ENTER);
-        Thread.sleep (4000);
-        String url = driver.getCurrentUrl();
+        //    System.setProperty("webdriver.chrome.driver", "/Users/vjaceslavsjermakovs/IdeaProjects/chrome driver/chromedriver113");
+        //    WebDriver driver = new ChromeDriver();
+        //    driver.get("https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&auth_url=https://accounts.google.com/o/oauth2/v2/auth&client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com&response_type=code&redirect_uri=https://rahulshettyacademy.com/getCourse.php");
+        //    driver.findElement(By.cssSelector("input[type='email']")).sendKeys("test.udemy14");
+        //    driver.findElement (By.cssSelector("input [type='email']")).sendKeys (Keys. ENTER);
+        //    Thread.sleep (3000);
+        //    driver.findElement(By.cssSelector("input [type='password']")).sendKeys ("Password4512");
+        //    driver.findElement (By.cssSelector("input [type='password']")). sendKeys (Keys. ENTER);
+        //    Thread.sleep (4000);
+        //    String url = driver.getCurrentUrl();
+        String url = "https://rahulshettyacademy.com/getCourse.php?code=4%2F0AbUR2VNZ6xqdcs5nOfFGpoaNZN8_Fk9F5CKY1vlj6S6BIrsQDhn5bk9IYY_vZ7bXVrEBUw&scope=email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&prompt=consent";
         String partialCode = url.split("code=")[1];
         String code = partialCode.split("&scope")[0];
         System.out.println(code);
-
         String accessTokenResponse =
                 given()
+                        .urlEncodingEnabled(false)
                         .queryParams("code", code)
                         .queryParams("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
                         .queryParams("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
