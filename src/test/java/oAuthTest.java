fc0fa8fe56fd08950c651213ed9f698bec84f747
vjaceslavsj <vjaceslavs.jermakovs@bta.lv>
2023-05-10 16:51:39 +0300

    Serialization and Deserialization dependencies

--- src/test/java/oAuthTest.java
+++ src/test/java/oAuthTest.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class oAuthTest {
 
     public static void main(String[] args) throws InterruptedException {
-        // TODO Auto-generated method stub
         //    System.setProperty("webdriver.chrome.driver", "/Users/vjaceslavsjermakovs/IdeaProjects/chrome driver/chromedriver113");
         //    WebDriver driver = new ChromeDriver();
         //    driver.get("https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&auth_url=https://accounts.google.com/o/oauth2/v2/auth&client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com&response_type=code&redirect_uri=https://rahulshettyacademy.com/getCourse.php");
