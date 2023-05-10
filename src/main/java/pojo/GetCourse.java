44af88157946a8a4ea7f9f88eef1d5f44e9d553b
vjaceslavsj <vjaceslavs.jermakovs@bta.lv>
2023-05-10 17:01:14 +0300

    Pojo class, getters and setter

---
+++ src/main/java/pojo/GetCourse.java
@@ -0,0 +1,58 @@
+package pojo;
+
+public class GetCourse {
+    private String url;
+    private String services;
+    private String expertise;
+    private String courses;
+    private String instructor;
+    private String linkedIn;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getServices() {
+        return services;
+    }
+
+    public String getExpertise() {
+        return expertise;
+    }
+
+    public String getCourses() {
+        return courses;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setServices(String services) {
+        this.services = services;
+    }
+
+    public void setExpertise(String expertise) {
+        this.expertise = expertise;
+    }
+
+    public void setCourses(String courses) {
+        this.courses = courses;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+}
